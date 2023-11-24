8e1b8c66cd4b3ec7aac679a561860285995e8ab6
AmyMcCann <amy.mccann.34@gmail.com>
2023-11-24 16:27:45 +0000

    Updated the design recipe and image

--- lib/receipt.py
+++ lib/receipt.py
@@ -10,14 +10,6 @@ class Receipt():
         for item in self.order.order_list:
             total += item[0].price * item[1]
         return total
-    
-    def format_total(self,total):
-        if type(total) == int:
-            return f"£{total}.00"
-        elif len(str(total).split(".")[1]) == 1:
-            return f"£{total}0"
-        else:
-            return f"£{total}"
 
     def get_receipt(self):
         order_confirmation = f"Order Confirmation:\n{self.order.view_order()}Total Order: {self.order.order_list[0][0].format_price(self.order_total())}"
